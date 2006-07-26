b5d4ff0f2ba6bec04356af03152118e157e9de93
hholzgraefe <hholzgraefe@1e5f01b0-2110-0410-979b-a52c7009c9cb>
2006-07-26 06:01:14 +0000

    auto-generate program list, assuming that each mysql_*.c maps to exactly one binary of the same basename

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+cp Makefile.base Makefile.am
+echo -n "bin_PROGRAMS = " >> Makefile.am
+for file in mysql*.c
+do
+  echo -n " "`basename $file .c` >> Makefile.am
+done
 aclocal
 autoheader
 libtoolize --force
